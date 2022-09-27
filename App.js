d99890ed9072534cd93950abef4b11397fbe1e5a
Abraham E. Tavarez <abetavarez@icloud.com>
2022-09-26 20:59:18 -0400

    loding image from device

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import logo from './assets/logo.png';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  let openImagePickerAsync = async () => {
+    let pickerResults = await ImagePicker.launchImageLibraryAsync();
+
+    if (pickerResults.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResults.uri });
+  };
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Image source={logo} style={styles.logo} />
+
+      <Text style={styles.instructions}>
+        To share a photo from your phone with a friend, just press the button
+        below!
+      </Text>
+
+      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
+        <Text style={styles.btnText}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -15,6 +49,31 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 10
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10
+  },
+  button: {
+    backgroundColor: 'royalblue',
+    padding: 20,
+    borderRadius: 5
+  },
+  btnText: {
+    fontSize: 20,
+    color: '#fff'
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain'
+  }
 });
